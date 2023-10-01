94f198ab39a18ce4d9aa061db8cdcb5da5c1d9de
TheFrisb <bedzovski@yahoo.com>
2023-10-01 12:14:19 +0200

    removed drf

--- cart/urls.py
+++ cart/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from . import views  # Django rest framework views
+from . import views
 
-from . import views_without_DjangoRestFramework
+
 urlpatterns = [
-    path('add-to-cart/', views_without_DjangoRestFramework.addToCart, name="addToCart"),
-    path('update-cart-item/', views_without_DjangoRestFramework.updateCartItem, name="updateCartItem"),
-    path('remove-from-cart/', views_without_DjangoRestFramework.removeFromCart, name="removeFromCart"),
+    path('add-to-cart/', views.addToCart, name="addToCart"),
+    path('update-cart-item/', views.updateCartItem, name="updateCartItem"),
+    path('remove-from-cart/', views.removeFromCart, name="removeFromCart"),
 ]
 
-# urlpatterns = [
-#     path('actions/', views.CartView.as_view(), name='cartActions'),
-# ]
