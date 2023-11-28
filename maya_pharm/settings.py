44289844908c92bd5819ffd92f1fc17d9b9828cf
TheFrisb <bedzovski@yahoo.com>
2023-11-29 00:40:41 +0100

    R

--- maya_pharm/settings.py
+++ maya_pharm/settings.py
@@ -92,24 +92,26 @@ WSGI_APPLICATION = 'maya_pharm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'majafarm',
+            'USER': 'majafarmuser',
+            'PASSWORD': 'majafarmuserSecure123',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #         'NAME': 'majafarm',
-    #         'USER': 'majafarmuser',
-    #         'PASSWORD': 'majafarmuserSecure123',
-    #         'HOST': 'localhost',
-    #         'PORT': '',
-    #     }
-    # }
+
 
 
 # Password validation
